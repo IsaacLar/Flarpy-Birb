c477cedeae1db57059c0ca16c5b9a9214258b6ad
IsaacLar <I.g.larssen2@newcastle.ac.uk>
2025-11-13 21:17:53 +0000

    Added Bird object and Space key input
    Added Bird game object and code to allow user to apply vertical velocity with space key

---
+++ Flarpy Birb/Assets/Scripts/BirdScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+
+    public int upwardsVelocity;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var keyboard = Keyboard.current;
+
+        if (keyboard.spaceKey.wasPressedThisFrame)
+        {
+            myRigidbody.linearVelocity = Vector2.up * upwardsVelocity;
+            print(myRigidbody.linearVelocity);
+        }
+    }
+}
