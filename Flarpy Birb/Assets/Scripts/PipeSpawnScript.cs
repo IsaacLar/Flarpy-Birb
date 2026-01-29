ad2b438df9e3863eebaf615d92f6d7081f065d94
IsaacLar <I.g.larssen2@newcastle.ac.uk>
2026-01-29 12:48:15 +0000

    Added Score UI and begun implementation

--- Flarpy Birb/Assets/Scripts/PipeSpawnScript.cs
+++ Flarpy Birb/Assets/Scripts/PipeSpawnScript.cs
@@ -24,22 +24,29 @@ public class PipeSpawnScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //If timer hasn't passed the respective time between pipes
         if ( timer < spawnRate)
-        {
+        {   
+            //Increment timer
             timer += Time.deltaTime;
         } else
-        {
+        {   
+            //Spawn a pipe
             spawnPipe();
+            //Pick a random time to spawn next pipe
             spawnRate = Random.Range(3, 5);
+            //Reset timer
             timer = 0;
         }           
     }
 
     void spawnPipe()
     {
+        //Calculate lowest and highest points
         lowestPoint = transform.position.y - heightOffset;
         highestPoint = transform.position.y + heightOffset;
 
+        //Spawn new pipe
         Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
     }
 
