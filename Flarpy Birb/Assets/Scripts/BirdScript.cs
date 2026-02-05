8155b1a15bab06071eb10bf7145f228e8c1e207e
IsaacLar <I.g.larssen2@newcastle.ac.uk>
2026-02-05 12:22:14 +0000

    Added game over screen and logic

--- Flarpy Birb/Assets/Scripts/BirdScript.cs
+++ Flarpy Birb/Assets/Scripts/BirdScript.cs
@@ -4,22 +4,32 @@ using UnityEngine.InputSystem;
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
+    public LogicScript logic;
 
     public int upwardsVelocity;
+    public bool birdIsAlive = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Input using the new Input System Package
         var keyboard = Keyboard.current;
 
         if (keyboard.spaceKey.wasPressedThisFrame)
         {
             myRigidbody.linearVelocity = Vector2.up * upwardsVelocity;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        birdIsAlive = false;
+        logic.gameOver();
     }
 }
